5c16aa0ccc6d4e3b97e20e9a59254da0031244bc
Fourbasse <julien.bensliman@gmail.com>
2023-06-27 11:52:04 +0200

    ajout fonction pour récupérer les donnée de tableaux

--- main.py
+++ main.py
@@ -34,9 +34,44 @@ def connexion(driver):
 def get_mantis_from_filter(driver,filter):
     select = driver.find_element(By.ID, 'filter-bar-query-id')
     obj_select = Select(select)
-    
-#Initialisation du dico de données
-data = {}
+
+    obj_select.select_by_visible_text(filter)
+    driver.implicitly_wait(10)
+    return driver
+
+
+def get_table_data (driver):
+    Table_obj = driver.find_element(By.ID, "buglist")
+    data = {}
+    #On récupère les lignes
+    obj_lines = Table_obj.find_element(By.TAG_NAME, "tbody").find_elements(By.TAG_NAME, "tr")
+    #On parcourt les lignes
+    for row in obj_lines:
+        cells = row.find_elements(By.TAG_NAME, "td") #On va récupèrer les cellules du tableau
+        link = row.find_elements(By.TAG_NAME, 'a') #ici on récupère les liens (/!\ yen a 2
+        try:
+            nb_mantis = cells[3].text.replace('0', '')
+            mant_cat = cells[6].text
+            statut_list = cells[8].text.replace(' (','(').split("(")[:-1]
+            statut = ""
+            #Petite boucle pour renseigner le statut sous forme de string
+            for i in statut_list:
+                statut += i
+            affect_to = cells[8].text.replace(' (', '(').split("(")[-1].replace(')','')
+
+            #on ajoute les infos dans le dico
+            data[nb_mantis] = {}
+            data[nb_mantis]["Catégorie"] = mant_cat.replace("[","").replace("]","")
+            data[nb_mantis]["Statut"] = statut
+            data[nb_mantis]["Affecte a"] = affect_to
+
+            href = url.replace('my_view_page.php', 'view.php?id='+nb_mantis)
+            data[nb_mantis]["url"] = href
+        except Exception as e:
+            print(e)
+            continue
+
+    return data
 
 
 #On paramètre le driver
@@ -48,36 +83,14 @@ connect_page = connexion(driver)
 #on clique sur "afficher les bugs"
 
 elem = driver.find_element(By.XPATH,"//*[@id='sidebar']/ul/li[2]/a").click()
-
+#
+get_mantis_from_filter(driver, 'Open Issues')
+open_issue_data = get_table_data(driver)
 #On récupère la table qui contient la liste des tickets
-# Table_obj = driver.find_element(By.ID, "buglist")
-# #On récupère les lignes
-# obj_lines = Table_obj.find_element(By.TAG_NAME, "tbody").find_elements(By.TAG_NAME, "tr")
-# #On parcourt les lignes
-# for row in obj_lines:
-#     ROW = []
-#     cells = row.find_elements(By.TAG_NAME, "td") #On va récupèrer les cellules du tableau
-#     link = row.find_elements(By.TAG_NAME, 'a') #ici on récupère les liens (/!\ yen a 2
-#     nb_mantis = cells[3].text.replace('0', '')
-#     mant_cat = cells[6].text
-#     statut_list = cells[8].text.replace(' (','(').split("(")[:-1]
-#     statut = ""
-#     #Petite boucle pour renseigner le statut sous forme de string
-#     for i in statut_list:
-#         statut += i
-#     affect_to = cells[8].text.replace(' (', '(').split("(")[-1].replace(')','')
 #
-#     #on ajoute les infos dans le dico
-#     data[nb_mantis] = {}
-#     data[nb_mantis]["Catégorie"] = mant_cat
-#     data[nb_mantis]["Statut"] = statut
-#     data[nb_mantis]["Affecte a"] = affect_to
-#
-#     href = url.replace('my_view_page.php', 'view.php?id='+nb_mantis)
-#     data[nb_mantis]["url"] = href
 
 driver.close()
-
+print(open_issue_data)
 # for key,value in data.items():
 #     driver = webdriver.Firefox()
 #     driver.get(value["url"])
